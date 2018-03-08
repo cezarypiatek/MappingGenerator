cdd753c31a6882df09bf4392bd1005a97cc14704
Cezary Piątek <cepi@ailleron.com>
2018-03-08 21:49:08 +0100

    Extract method for finding containing expression

---
+++ MappingGenerator/MappingGenerator/MappingGenerator/SyntaxHelper.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace MappingGenerator
+{
+    static class  SyntaxHelper
+    {
+        public static T FindContainer<T>(this SyntaxNode tokenParent) where T:SyntaxNode
+        {
+            if (tokenParent is T invocation)
+            {
+                return invocation;
+            }
+
+            return tokenParent.Parent == null ? null : FindContainer<T>(tokenParent.Parent);
+        }
+    }
+}

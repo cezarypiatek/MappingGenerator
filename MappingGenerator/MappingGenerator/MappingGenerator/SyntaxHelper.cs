0e92d62abc96357d592533713abe90f562f622c2
Cezary Piątek <cepi@ailleron.com>
2018-04-06 22:37:00 +0200

    #13 Fix detecting syntax construction order

--- MappingGenerator/MappingGenerator/MappingGenerator/SyntaxHelper.cs
+++ MappingGenerator/MappingGenerator/MappingGenerator/SyntaxHelper.cs
@@ -12,6 +12,22 @@ namespace MappingGenerator
             }
 
             return tokenParent.Parent == null ? null : FindContainer<T>(tokenParent.Parent);
+        } 
+        
+        public static SyntaxNode FindNearestContainer<T1, T2>(this SyntaxNode tokenParent) where T1:SyntaxNode where T2:SyntaxNode
+        {
+            if (tokenParent is T1 t1)
+            {
+                return t1;
+            }
+            
+            if (tokenParent is T2 t2)
+            {
+                return t2;
+            }
+            
+
+            return tokenParent.Parent == null ? null : FindNearestContainer<T1, T2>(tokenParent.Parent);
         }
     }
 }

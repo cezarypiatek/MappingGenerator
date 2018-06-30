36ec4112c262b7c9bc8ccecef97010afac8311b9
Cezary Piątek <cepi@ailleron.com>
2018-06-30 18:36:08 +0200

    Add UT for MappingGeneratorCodeFixProvider

---
+++ MappingGenerator/MappingGenerator/MappingGenerator.Test/MappingGenerator/TestCaseData/003_MappingFromOneToAnotherParameter_FIXED.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections.ObjectModel;
+
+namespace MappingGenerator.Test.MappingGenerator.TestCaseData
+{
+    public class TestMapper
+    {
+        public static void Map(UserEntity src, UserDTO dst)
+        {
+            dst.Age = src.Age;
+            dst.Debs = src.Debs.Select(srcDeb => new AccountDTO()
+            {
+                BankName = srcDeb.BankName,
+                Number = srcDeb.Number
+            }).ToList();
+            dst.Source = new UserSourceDTO(providerName: src.Source.ProviderName, providerAddress: src.Source.ProviderAddress);
+            dst.ImageData = src.ImageData;
+            dst.LuckyNumbers = src.LuckyNumbers;
+            dst.Total = src.GetTotal();
+            dst.MainAddress = new AddressDTO()
+            {
+                City = src.MainAddress.City,
+                ZipCode = src.MainAddress.ZipCode,
+                Street = src.MainAddress.Street,
+                FlatNo = src.MainAddress.FlatNo,
+                BuildingNo = src.MainAddress.BuildingNo
+            };
+            dst.Addresses = src.Addresses.Select(srcAddresse => new AddressDTO()
+            {
+                City = srcAddresse.City,
+                ZipCode = srcAddresse.ZipCode,
+                Street = srcAddresse.Street,
+                FlatNo = srcAddresse.FlatNo,
+                BuildingNo = srcAddresse.BuildingNo
+            }).ToList().AsReadOnly();
+            dst.UnitId = src.Unit.Id;
+        }
+    }
+
+    public class UserDTO
+    {
+        public string FirstName { get;  }
+        public string LastName { get; private set; }
+        public int Age { get; set; }
+        public int Cash { get;}
+        public AccountDTO Account { get; private set; }
+        public List<AccountDTO> Debs { get; set; }
+        public UserSourceDTO Source { get; set; }
+        public string Login { get; set; }
+        public byte[] ImageData { get; set; }
+        public List<int> LuckyNumbers { get; set; }
+        public int Total { get; set; }
+        public AddressDTO MainAddress { get; set; }
+        public ReadOnlyCollection<AddressDTO> Addresses { get; set; }
+        public int UnitId { get; set; }
+    }
+
+    public class UserSourceDTO
+    {
+        public string ProviderName { get; set; }
+        public string ProviderAddress { get; set; }
+
+        public UserSourceDTO(string providerName, string providerAddress)
+        {
+            ProviderName = providerName;
+            ProviderAddress = providerAddress;
+        }
+    }
+    
+    public class AccountDTO
+    {
+        public string BankName { get; set; }
+        public string Number { get; set; }
+    }
+
+    public class AddressDTO
+    {
+        public string City { get; set; }
+        public string ZipCode { get; set; }
+        public string Street { get; set; }
+        public string FlatNo { get; set; }
+        public string BuildingNo { get; set; }
+    }
+
+    //---- Entities
+
+    public class UserEntity
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public AccountEntity Account { get; set; }
+        public List<AccountEntity> Debs { get; set; }
+        public UserSourceEntity Source { get; set; }
+        public string Name { get;  }
+        public CustomerKind Kind { get; set; }
+        public double Cash { get; }
+        public byte[] ImageData { get; set; }
+        public List<int> LuckyNumbers { get; set; }
+        public AddressEntity MainAddress { get; set; }
+        public string AddressCity { get; set; }
+        public List<AddressEntity> Addresses { get; set; }
+        public UnitEntity Unit { get; set; }
+
+        public int GetTotal()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class AddressEntity
+    {   
+        public string City { get; set; }
+        public string ZipCode { get; set; }
+        public string Street { get; set; }
+        public string FlatNo { get; set; }
+        public string BuildingNo { get; set; }
+    }
+
+    public class UnitEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class UserSourceEntity
+    {
+        public string ProviderName { get; set; }
+        public string ProviderAddress { get; set; }
+    }
+
+    public class AccountEntity
+    {
+        public string BankName { get; set; }
+        public string Number { get; set; }
+    }
+}
--- MappingGenerator/MappingGenerator/MappingGenerator/MappingGeneratorAnalyzer.cs
+++ MappingGenerator/MappingGenerator/MappingGenerator/MappingGeneratorAnalyzer.cs
@@ -18,7 +18,7 @@ namespace MappingGenerator
         // You can change these strings in the Resources.resx file. If you do not want your analyzer to be localize-able, you can use regular strings for Title and MessageFormat.
         // See https://github.com/dotnet/roslyn/blob/master/docs/analyzers/Localizing%20Analyzers.md for more on localization
         private static readonly LocalizableString Title = new LocalizableResourceString(nameof(Resources.AnalyzerTitle), Resources.ResourceManager, typeof(Resources));
-        private static readonly LocalizableString MessageFormat = new LocalizableResourceString(nameof(Resources.AnalyzerMessageFormat), Resources.ResourceManager, typeof(Resources));
+        public static readonly LocalizableString MessageFormat = new LocalizableResourceString(nameof(Resources.AnalyzerMessageFormat), Resources.ResourceManager, typeof(Resources));
         private static readonly LocalizableString Description = new LocalizableResourceString(nameof(Resources.AnalyzerDescription), Resources.ResourceManager, typeof(Resources));
         private const string Category = "Usage";
 

c9b220d3ce252472f0af97ecbef0522f121a7aa9
Cezary Piątek <cepi@ailleron.com>
2018-06-30 11:20:19 +0200

    Add method to test code fix providers without custom analyzer

--- MappingGenerator/MappingGenerator/MappingGenerator.Test/Verifiers/CodeFixVerifier.cs
+++ MappingGenerator/MappingGenerator/MappingGenerator.Test/Verifiers/CodeFixVerifier.cs
@@ -47,6 +47,19 @@ namespace TestHelper
         }
 
         /// <summary>
+        /// Called to test a C# codefix when applied on the inputted string as a source
+        /// </summary>
+        /// <param name="oldSource">A class in the form of a string before the CodeFix was applied to it</param>
+        /// <param name="newSource">A class in the form of a string after the CodeFix was applied to it</param>
+        /// <param name="diagnosticId">Diagnostic id to fix</param>
+        /// <param name="codeFixIndex">Index determining which codefix to apply if there are multiple</param>
+        /// <param name="allowNewCompilerDiagnostics">A bool controlling whether or not the test will fail if the CodeFix introduces other warnings after being applied</param>
+        protected void VerifyCSharpFix(string oldSource, string newSource, string diagnosticId, int? codeFixIndex = null, bool allowNewCompilerDiagnostics = false)
+        {
+            VerifyFix(LanguageNames.CSharp, diagnosticId, GetCSharpCodeFixProvider(), oldSource, newSource, codeFixIndex, allowNewCompilerDiagnostics);
+        }
+
+        /// <summary>
         /// Called to test a VB codefix when applied on the inputted string as a source
         /// </summary>
         /// <param name="oldSource">A class in the form of a string before the CodeFix was applied to it</param>
@@ -117,6 +130,65 @@ namespace TestHelper
                 if (!analyzerDiagnostics.Any())
                 {
                     break;
+                }
+            }
+
+            //after applying all of the code fixes, compare the resulting string to the inputted one
+            var actual = GetStringFromDocument(document);
+            Assert.AreEqual(newSource, actual);
+        }
+        
+        
+        /// <summary>
+        /// General verifier for codefixes.
+        /// Creates a Document from the source string, then gets diagnostics on it and applies the relevant codefixes.
+        /// Then gets the string after the codefix is applied and compares it with the expected result.
+        /// Note: If any codefix causes new diagnostics to show up, the test fails unless allowNewCompilerDiagnostics is set to true.
+        /// </summary>
+        /// <param name="language">The language the source code is in</param>
+        /// /// <param name="diagnosticId">Diagnostic id to fix</param>
+        /// <param name="codeFixProvider">The codefix to be applied to the code wherever the relevant Diagnostic is found</param>
+        /// <param name="oldSource">A class in the form of a string before the CodeFix was applied to it</param>
+        /// <param name="newSource">A class in the form of a string after the CodeFix was applied to it</param>
+        /// <param name="codeFixIndex">Index determining which codefix to apply if there are multiple</param>
+        /// <param name="allowNewCompilerDiagnostics">A bool controlling whether or not the test will fail if the CodeFix introduces other warnings after being applied</param>
+        private void VerifyFix(string language, string diagnosticId, CodeFixProvider codeFixProvider, string oldSource, string newSource, int? codeFixIndex, bool allowNewCompilerDiagnostics)
+        {
+            var document = CreateDocument(oldSource, language);
+            var compilerDiagnostics = GetCompilerDiagnostics(document);
+            
+            foreach (var diagnosticToFix in compilerDiagnostics.Where(x => x.Id == diagnosticId))
+            {
+                var actions = new List<CodeAction>();
+                var context = new CodeFixContext(document, diagnosticToFix, (a, d) => actions.Add(a), CancellationToken.None);
+                codeFixProvider.RegisterCodeFixesAsync(context).Wait();
+
+                if (!actions.Any())
+                {
+                    break;
+                }
+
+                if (codeFixIndex != null)
+                {
+                    document = ApplyFix(document, actions.ElementAt((int)codeFixIndex));
+                    break;
+                }
+
+                document = ApplyFix(document, actions.ElementAt(0));
+
+                var newCompilerDiagnostics = GetNewDiagnostics(compilerDiagnostics, GetCompilerDiagnostics(document));
+
+                //check if applying the code fix introduced any new compiler diagnostics
+                if (!allowNewCompilerDiagnostics && newCompilerDiagnostics.Any())
+                {
+                    // Format and get the compiler diagnostics again so that the locations make sense in the output
+                    document = document.WithSyntaxRoot(Formatter.Format(document.GetSyntaxRootAsync().Result, Formatter.Annotation, document.Project.Solution.Workspace));
+                    newCompilerDiagnostics = GetNewDiagnostics(compilerDiagnostics, GetCompilerDiagnostics(document));
+
+                    Assert.IsTrue(false,
+                        string.Format("Fix introduced new compiler diagnostics:\r\n{0}\r\n\r\nNew document:\r\n{1}\r\n",
+                            string.Join("\r\n", newCompilerDiagnostics.Select(d => d.ToString())),
+                            document.GetSyntaxRootAsync().Result.ToFullString()));
                 }
             }
 

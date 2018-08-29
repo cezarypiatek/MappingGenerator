3097250e9cc1d63349ee0e2e0577117d7ff91854
Cezary Piatek <cezarypiatek@live.com>
2018-08-29 22:05:50 +0200

    Add UT, implement ICloneable pattern. Replace methods when already exist/

---
+++ MappingGenerator/MappingGenerator/MappingGenerator.Test/ImplementCloneMethod/ImplementCloneMethodTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeRefactorings;
+using NUnit.Framework;
+using RoslynNUnitLight;
+
+namespace MappingGenerator.Test.ImplementCloneMethod
+{
+    public class ImplementCloneMethodTests :  CodeRefactoringTestFixture
+    {
+        [Test]
+        public void should_be_able_to_generate_deep_clone_method()
+        {
+            var test = ImplementCloneMethodTestCases._001_DeepClone;
+            var fixedCode = ImplementCloneMethodTestCases._001_DeepClone_FIXED;
+            TestCodeRefactoring(test, fixedCode);
+        }
+
+        protected override string LanguageName => LanguageNames.CSharp;
+        protected override CodeRefactoringProvider CreateProvider()
+        {
+            return new ImplementCloneMethodRefactoring();
+        }
+    }
+}

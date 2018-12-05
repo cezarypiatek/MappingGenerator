7b44aa98088f634ab7fb94526a06e04c25949b63
Cezary Piatek <cezarypiatek@live.com>
2018-12-05 20:01:10 +0100

    Replace standard test helpers with RoslynNunitLight references via git submodule

--- MappingGenerator/MappingGenerator/MappingGenerator.Test/Splatting/SplattingTests.cs
+++ MappingGenerator/MappingGenerator/MappingGenerator.Test/Splatting/SplattingTests.cs
@@ -1,43 +1,45 @@
 using Microsoft.CodeAnalysis.CodeFixes;
-using TestHelper;
 using MappingGenerator.Test.Splatting;
+using Microsoft.CodeAnalysis;
 using NUnit.Framework;
+using RoslynNUnitLight;
 
 namespace MappingGenerator.Test
 {
-    public class SplattingTests : CodeFixVerifier
+    public class SplattingTests : CodeFixTestFixture
     {
         [Test]
         public void should_be_able_to_generate_splatting_for_method_invocation_with_regular_parameters()
         {
-            VerifyCSharpFix(SplattingTestCases._001_SplattingInMethodInvocation, SplattingTestCases._001_SplattingInMethodInvocation_FIXED, SplattingCodeFixProvider.CS7036, 0);
+            TestCodeFix(SplattingTestCases._001_SplattingInMethodInvocation, SplattingTestCases._001_SplattingInMethodInvocation_FIXED, SplattingCodeFixProvider.CS7036, 0);
         }
 
         [Test]
         public void should_be_able_to_generate_splatting_for_method_invocation_with_named_parameters()
         {
-            VerifyCSharpFix(SplattingTestCases._001_SplattingInMethodInvocation, SplattingTestCases._001_SplattingInMethodInvocationWithNamedParameters_FIXED, SplattingCodeFixProvider.CS7036, 1);
+            TestCodeFix(SplattingTestCases._001_SplattingInMethodInvocation, SplattingTestCases._001_SplattingInMethodInvocationWithNamedParameters_FIXED, SplattingCodeFixProvider.CS7036, 1);
         }
 
         [Test]
         public void should_be_able_to_generate_splatting_for_constructor_invocation_with_regular_parameters()
         {
-            VerifyCSharpFix(SplattingTestCases._002_SplattingInConstructorInvocation, SplattingTestCases._002_SplattingInConstructorInvocation_FIXED, SplattingCodeFixProvider.CS7036, 0);
+            TestCodeFix(SplattingTestCases._002_SplattingInConstructorInvocation, SplattingTestCases._002_SplattingInConstructorInvocation_FIXED, SplattingCodeFixProvider.CS7036, 0);
         }
 
         [Test]
         public void should_be_able_to_generate_splatting_for_constructor_invocation_with_named_parameters()
         {
-            VerifyCSharpFix(SplattingTestCases._002_SplattingInConstructorInvocation, SplattingTestCases._002_SplattingInConstructorInvocationWithNamedParameters_FIXED, SplattingCodeFixProvider.CS7036, 1);
+            TestCodeFix(SplattingTestCases._002_SplattingInConstructorInvocation, SplattingTestCases._002_SplattingInConstructorInvocationWithNamedParameters_FIXED, SplattingCodeFixProvider.CS7036, 1);
         }
 
         [Test]
         public void should_be_able_to_generate_splatting_for_best_method_overload()
         {
-            VerifyCSharpFix(SplattingTestCases._003_SplattingWithBestOverloadMatch, SplattingTestCases._003_SplattingWithBestOverloadMatch_FIXED, SplattingCodeFixProvider.CS1501, 1);
+            TestCodeFix(SplattingTestCases._003_SplattingWithBestOverloadMatch, SplattingTestCases._003_SplattingWithBestOverloadMatch_FIXED, SplattingCodeFixProvider.CS1501, 1);
         }
 
-        protected override CodeFixProvider GetCSharpCodeFixProvider()
+        protected override string LanguageName => LanguageNames.CSharp;
+        protected override CodeFixProvider CreateProvider()
         {
             return new SplattingCodeFixProvider();
         }
--- MappingGenerator/MappingGenerator/MappingGenerator.Test/UseLocalVariablesAsParameters/UserLocalVariablesAsParametersTests.cs
+++ MappingGenerator/MappingGenerator/MappingGenerator.Test/UseLocalVariablesAsParameters/UserLocalVariablesAsParametersTests.cs
@@ -1,38 +1,42 @@
 using Microsoft.CodeAnalysis.CodeFixes;
 using TestHelper;
 using MappingGenerator.Test.UseLocalVariablesAsParameters;
+using Microsoft.CodeAnalysis;
 using NUnit.Framework;
+using RoslynNUnitLight;
 
 namespace MappingGenerator.Test
 {
     
-    public class UseLocalVariablesAsParameter : CodeFixVerifier
+    public class UseLocalVariablesAsParameter : CodeFixTestFixture
     {
         [Test]
         public void should_be_able_to_complete_method_invocation_with_local_variables_as_regular_parameters()
         {
-            VerifyCSharpFix(UseLocalVariablesTestCases._001_UserLocaVariablesToCompleteMethodInvocation, UseLocalVariablesTestCases._001_UserLocaVariablesToCompleteMethodInvocation_Fixed, UseLocalVariablesAsParameterCodeFixProvider.CS7036, 0);
+            TestCodeFix(UseLocalVariablesTestCases._001_UserLocaVariablesToCompleteMethodInvocation, UseLocalVariablesTestCases._001_UserLocaVariablesToCompleteMethodInvocation_Fixed, UseLocalVariablesAsParameterCodeFixProvider.CS7036, 0);
         }
 
         [Test]
         public void should_be_able_to_complete_method_invocation_with_local_variables_as_named_parameters()
         {
-            VerifyCSharpFix(UseLocalVariablesTestCases._001_UserLocaVariablesToCompleteMethodInvocation, UseLocalVariablesTestCases._001_UserLocaVariablesToCompleteMethodInvocationWithNamedParameters_Fixed, UseLocalVariablesAsParameterCodeFixProvider.CS7036, 1);
+            TestCodeFix(UseLocalVariablesTestCases._001_UserLocaVariablesToCompleteMethodInvocation, UseLocalVariablesTestCases._001_UserLocaVariablesToCompleteMethodInvocationWithNamedParameters_Fixed, UseLocalVariablesAsParameterCodeFixProvider.CS7036, 1);
         }
 
         [Test]
         public void should_be_able_to_complete_constructor_invocation_with_local_variables_as_regular_parameters()
         {
-            VerifyCSharpFix(UseLocalVariablesTestCases._002_UserLocaVariablesToCompleteConstructorInvocation, UseLocalVariablesTestCases._002_UserLocaVariablesToCompleteConstructorInvocation_Fixed, SplattingCodeFixProvider.CS7036, 0);
+            TestCodeFix(UseLocalVariablesTestCases._002_UserLocaVariablesToCompleteConstructorInvocation, UseLocalVariablesTestCases._002_UserLocaVariablesToCompleteConstructorInvocation_Fixed, SplattingCodeFixProvider.CS7036, 0);
         }
 
         [Test]
         public void should_be_able_to_complete_constructor_invocation_with_local_variables_as_named_parameters()
         {
-            VerifyCSharpFix(UseLocalVariablesTestCases._002_UserLocaVariablesToCompleteConstructorInvocation, UseLocalVariablesTestCases._002_UserLocaVariablesToCompleteConstructorInvocationWithNamedParameters_Fixed, SplattingCodeFixProvider.CS7036, 1);
+            TestCodeFix(UseLocalVariablesTestCases._002_UserLocaVariablesToCompleteConstructorInvocation, UseLocalVariablesTestCases._002_UserLocaVariablesToCompleteConstructorInvocationWithNamedParameters_Fixed, SplattingCodeFixProvider.CS7036, 1);
         }
+        
+        protected override string LanguageName => LanguageNames.CSharp;
 
-        protected override CodeFixProvider GetCSharpCodeFixProvider()
+        protected override CodeFixProvider CreateProvider()
         {
             return new UseLocalVariablesAsParameterCodeFixProvider();
         }

b6b71424d8ae03fcfa091288dc61fbe7877c07e0
Cezary Piatek <cezarypiatek@live.com>
2019-04-27 09:30:21 +0200

    #76: Refactoring: Extract implementors from MappingGeneratorRefactoring. Move MappingGeneratorRefactoring to the dedicated subfolder.

---
+++ MappingGenerator/MappingGenerator/MappingGenerator/Features/Refactorings/Mapping/MappingImplementors/UpdateSecondParameterMappingMethodImplementor.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Editing;
+
+namespace MappingGenerator.Features.Refactorings.Mapping.MappingImplementors
+{
+    class UpdateSecondParameterMappingMethodImplementor:IMappingMethodImplementor
+    {
+        public bool CanImplement(IMethodSymbol methodSymbol)
+        {
+            if (SymbolHelper.IsConstructor(methodSymbol))
+            {
+                return false;
+            }
+            return methodSymbol.Parameters.Length == 2 && methodSymbol.ReturnsVoid;
+        }
+
+        public IEnumerable<SyntaxNode> GenerateImplementation(IMethodSymbol methodSymbol, SyntaxGenerator generator, SemanticModel semanticModel)
+        {
+            var mappingEngine = new MappingEngine(semanticModel, generator, methodSymbol.ContainingAssembly);
+            var source = methodSymbol.Parameters[0];
+            var target = methodSymbol.Parameters[1];
+            var targets = ObjectHelper.GetFieldsThaCanBeSetPublicly(target.Type, methodSymbol.ContainingAssembly);
+            var sourceFinder = new ObjectMembersMappingSourceFinder(source.Type, generator.IdentifierName(source.Name), generator);
+            return mappingEngine.MapUsingSimpleAssignment(generator, targets, sourceFinder, globalTargetAccessor: generator.IdentifierName(target.Name));
+        }
+    }
+}
---
+++ MappingGenerator/MappingGenerator/MappingGenerator/Features/Refactorings/Mapping/MappingImplementors/UpdateThisObjectSingleParameterMappingMethodImplementor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Editing;
+
+namespace MappingGenerator.Features.Refactorings.Mapping.MappingImplementors
+{
+    class UpdateThisObjectSingleParameterMappingMethodImplementor:IMappingMethodImplementor
+    {
+        public bool CanImplement(IMethodSymbol methodSymbol)
+        {
+            if (SymbolHelper.IsConstructor(methodSymbol))
+            {
+                return false;
+            }
+            return methodSymbol.Parameters.Length == 1 && methodSymbol.ReturnsVoid;
+        }
+
+        public IEnumerable<SyntaxNode> GenerateImplementation(IMethodSymbol methodSymbol, SyntaxGenerator generator, SemanticModel semanticModel)
+        {
+            var mappingEngine = new MappingEngine(semanticModel, generator, methodSymbol.ContainingAssembly);
+            var source = methodSymbol.Parameters[0];
+            var sourceFinder = new ObjectMembersMappingSourceFinder(source.Type, generator.IdentifierName(source.Name), generator);
+            var targets = ObjectHelper.GetFieldsThaCanBeSetPrivately(methodSymbol.ContainingType);
+            return mappingEngine.MapUsingSimpleAssignment(generator, targets, sourceFinder);
+        }
+    }
+}

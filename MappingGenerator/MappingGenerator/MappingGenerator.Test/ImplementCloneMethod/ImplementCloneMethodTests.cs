6f49023e4e34e7c2d372a9d24470f698b7353024
cezarypiatek <cezarypiatek@live.com>
2019-08-31 22:48:58 +0200

    Change RoslynNUnitLight to RoslynTestKit

--- MappingGenerator/MappingGenerator/MappingGenerator.Test/ImplementCloneMethod/ImplementCloneMethodTests.cs
+++ MappingGenerator/MappingGenerator/MappingGenerator.Test/ImplementCloneMethod/ImplementCloneMethodTests.cs
@@ -4,7 +4,7 @@ using System.Text;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CodeRefactorings;
 using NUnit.Framework;
-using RoslynNUnitLight;
+using RoslynTestKit;
 
 namespace MappingGenerator.Test.ImplementCloneMethod
 {

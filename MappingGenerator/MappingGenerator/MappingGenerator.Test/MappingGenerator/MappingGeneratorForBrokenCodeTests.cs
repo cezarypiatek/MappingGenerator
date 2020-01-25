09ad6f214886681f148deda7e17f98e4d0f77394
Cezary Piątek <cezarypiatek@live.com>
2020-01-25 15:59:38 +0100

    Fix test cases

---
+++ MappingGenerator/MappingGenerator/MappingGenerator.Test/MappingGenerator/MappingGeneratorForBrokenCodeTests.cs
@@ -0,0 +1,22 @@
+﻿using MappingGenerator.Features.Refactorings.Mapping;
+using MappingGenerator.Test.MappingGenerator;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeRefactorings;
+using NUnit.Framework;
+using RoslynTestKit;
+
+namespace MappingGenerator.Test.Mapping
+{
+    public class MappingGeneratorForBrokenCodeTests : CodeRefactoringTestFixture
+    {
+        protected override string LanguageName => LanguageNames.CSharp;
+        protected override CodeRefactoringProvider CreateProvider() => new MappingGeneratorRefactoring();
+        protected override bool FailWhenInputContainsErrors => false;
+
+        [Test]
+        public void should_be_able_to_replace_invalid_body_with_mapping_body()
+        {
+            TestCodeRefactoring(MappingGeneratorTestCases._024_InvalidSyntaxPureMappingMethod, MappingGeneratorTestCases._024_InvalidSyntaxPureMappingMethod_FIXED);
+        }
+    }
+}

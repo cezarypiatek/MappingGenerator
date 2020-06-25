cda684d767a5e78ae2a1b1bbfd15942babba7991
Cezary Piątek <cezarypiatek@live.com>
2020-06-25 23:40:10 +0200

    #129: Remove duplicated mapping targed caused by the inheritance

--- MappingGenerator/MappingGenerator/MappingGenerator/RoslynHelpers/MappingTargetHelper.cs
+++ MappingGenerator/MappingGenerator/MappingGenerator/RoslynHelpers/MappingTargetHelper.cs
@@ -7,20 +7,29 @@ namespace MappingGenerator.RoslynHelpers
 {
     public static class MappingTargetHelper
     {
+        private static readonly NameEqualityComparer nameEqualityComparer = new NameEqualityComparer();
         public static IReadOnlyCollection<IObjectField> GetFieldsThaCanBeSetFromConstructor(ITypeSymbol type, MappingContext mappingContext)
         {
-            return type.GetObjectFields().Where(x=>x.CanBeSetInConstructor(type, mappingContext)).ToList();
+            return type.GetObjectFields().Where(x=>x.CanBeSetInConstructor(type, mappingContext)).Distinct(nameEqualityComparer).ToList();
         }
 
         public static IReadOnlyCollection<IObjectField> GetFieldsThaCanBeSetPublicly(ITypeSymbol type,
             MappingContext mappingContext)
         {
-            return type.GetObjectFields().Where(x => x.CanBeSet(type, mappingContext)).ToList();
+            return type.GetObjectFields().Where(x => x.CanBeSet(type, mappingContext)).Distinct(nameEqualityComparer).ToList();
         }
 
         public static IReadOnlyCollection<IObjectField> GetFieldsThaCanBeSetPrivately(ITypeSymbol type, MappingContext mappingContext)
         {
-            return type.GetObjectFields().Where(x => x.CanBeSet(type, mappingContext)).ToList();
+            return type.GetObjectFields().Where(x => x.CanBeSet(type, mappingContext)).Distinct(nameEqualityComparer).ToList(); 
         }
+
+        class NameEqualityComparer : IEqualityComparer<IObjectField>
+        {
+            public bool Equals(IObjectField x, IObjectField y) => x?.Name == y?.Name;
+
+            public int GetHashCode(IObjectField obj) => obj.Name.GetHashCode();
+        }
+
     }
 }

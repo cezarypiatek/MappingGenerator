621928d4dca43d05767292228c018e9ffa84719d
Cezary Piątek <cezarypiatek@live.com>
2020-09-01 19:52:55 +0200

    Remove dependency to CSharpExtensions

--- MappingGenerator/MappingGenerator/MappingGenerator/Mappings/MappingElement.cs
+++ MappingGenerator/MappingGenerator/MappingGenerator/Mappings/MappingElement.cs
@@ -1,10 +1,8 @@
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
-using SmartAnalyzers.CSharpExtensions.Annotations;
 
 namespace MappingGenerator.Mappings
 {
-    [InitOnly]
     public class MappingElement
     {
         public ExpressionSyntax Expression { get; set; }

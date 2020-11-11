24d7e55bc06690a5b72b1297fe58c414fdf4553e
Cezary Piątek <cezarypiatek@live.com>
2020-11-11 20:51:01 +0100

    #146: Reuse object instance while generating explicit conversion

--- MappingGenerator/MappingGenerator/MappingGenerator/Features/CodeFixes/ExplicitConversionCodeFixProvider.cs
+++ MappingGenerator/MappingGenerator/MappingGenerator/Features/CodeFixes/ExplicitConversionCodeFixProvider.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Composition;
 using System.Linq;
@@ -5,10 +6,13 @@ using System.Threading;
 using System.Threading.Tasks;
 using MappingGenerator.Mappings;
 using MappingGenerator.Mappings.SourceFinders;
+using MappingGenerator.RoslynHelpers;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CodeActions;
 using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Editing;
 
 namespace MappingGenerator.Features.CodeFixes
 {
@@ -16,6 +20,7 @@ namespace MappingGenerator.Features.CodeFixes
     public class ExplicitConversionCodeFixProvider : CodeFixProvider
     {
         private const string title = "Generate explicit conversion";
+        private const string title2 = "Generate explicit conversion (Try re-use instance)";
         public const string CS0029 = nameof(CS0029);
         public const string CS0266 = nameof(CS0266);
 
@@ -43,7 +48,12 @@ namespace MappingGenerator.Features.CodeFixes
             switch (statement)
             {
                 case AssignmentExpressionSyntax assignmentExpression:
-                    context.RegisterCodeFix(CodeAction.Create(title: title, createChangedDocument: c => GenerateExplicitConversion(context.Document, assignmentExpression, c), equivalenceKey: title), diagnostic);
+                    context.RegisterCodeFix(CodeAction.Create(title: title, createChangedDocument: c => GenerateExplicitConversion(context.Document, assignmentExpression, false, c), equivalenceKey: title), diagnostic);
+
+                    if (assignmentExpression.Parent is InitializerExpressionSyntax || assignmentExpression.Parent is StatementSyntax)
+                    {
+                        context.RegisterCodeFix(CodeAction.Create(title: title2, createChangedDocument: c => GenerateExplicitConversion(context.Document, assignmentExpression, true, c), equivalenceKey: title), diagnostic);
+                    }
                     break;
                 case ReturnStatementSyntax returnStatement:
                     context.RegisterCodeFix(CodeAction.Create(title: title, createChangedDocument: c => GenerateExplicitConversion(context.Document, returnStatement, c), equivalenceKey: title), diagnostic);
@@ -54,13 +64,32 @@ namespace MappingGenerator.Features.CodeFixes
             }
         }
 
-        private async Task<Document> GenerateExplicitConversion(Document document, AssignmentExpressionSyntax assignmentExpression, CancellationToken cancellationToken)
+        private async Task<Document> GenerateExplicitConversion(Document document, AssignmentExpressionSyntax assignmentExpression, bool reUseInstance, CancellationToken cancellationToken)
         {
             var (mappingEngine, semanticModel) = await CreateMappingEngine(document, cancellationToken).ConfigureAwait(false);
             var sourceType = mappingEngine.GetExpressionTypeInfo(assignmentExpression.Right).GetAnnotatedType();
             var destinationType = mappingEngine.GetExpressionTypeInfo(assignmentExpression.Left).GetAnnotatedType();
             var mappingContext = new MappingContext(assignmentExpression, semanticModel);
-            var mappingExpression = await mappingEngine.MapExpression(assignmentExpression.Right.WithoutTrivia(), sourceType, destinationType, mappingContext).ConfigureAwait(false); 
+            var mappingExpression = await mappingEngine.MapExpression(assignmentExpression.Right.WithoutTrivia(), sourceType, destinationType, mappingContext).ConfigureAwait(false);
+
+            if (reUseInstance && mappingExpression is ObjectCreationExpressionSyntax objectCreation && objectCreation.Initializer is {}  initializer && initializer.Expressions.Count > 0 )
+            {
+                if (assignmentExpression.Parent is InitializerExpressionSyntax)
+                {
+                    return await ReplaceNode(document, assignmentExpression, assignmentExpression.WithRight(initializer), cancellationToken).ConfigureAwait(false);
+                }
+                else
+                {
+                    var targetAccessor = assignmentExpression.Left;
+                    var newInitExpressions = initializer.Expressions.OfType<AssignmentExpressionSyntax>().Select(expr =>
+                    {
+                        var left = SyntaxFactory.MemberAccessExpression(SyntaxKind.SimpleMemberAccessExpression, targetAccessor, (SimpleNameSyntax) expr.Left);
+                        return expr.WithLeft(left).AsStatement();
+                    }).ToList();
+                    return await ReplaceNodes(document, assignmentExpression.Parent, newInitExpressions, cancellationToken).ConfigureAwait(false);
+                }
+            }
+
             return await ReplaceNode(document, assignmentExpression, assignmentExpression.WithRight(mappingExpression), cancellationToken).ConfigureAwait(false);
         }
 
@@ -92,7 +121,14 @@ namespace MappingGenerator.Features.CodeFixes
         private static async Task<Document> ReplaceNode(Document document, SyntaxNode oldNode, SyntaxNode newNode, CancellationToken cancellationToken)
         {
             var root = await document.GetSyntaxRootAsync(cancellationToken).ConfigureAwait(false);
-            var newRoot = root!.ReplaceNode(oldNode, newNode);
+            var newRoot = root.ReplaceNode(oldNode, newNode);
+            return document.WithSyntaxRoot(newRoot);
+        }
+        
+        private static async Task<Document> ReplaceNodes(Document document, SyntaxNode oldNode, IReadOnlyList<SyntaxNode> newNodes, CancellationToken cancellationToken)
+        {
+            var root = await document.GetSyntaxRootAsync(cancellationToken).ConfigureAwait(false);
+            var newRoot = root.ReplaceNode(oldNode, newNodes);
             return document.WithSyntaxRoot(newRoot);
         }
 
